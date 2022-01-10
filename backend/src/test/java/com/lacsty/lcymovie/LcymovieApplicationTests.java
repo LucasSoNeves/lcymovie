67215d305353e82c72ac526d1c5de27032c04f07
Lucas de Sousa <lucas.neves.ead@gmail.com>
2022-01-10 10:03:32 -0300

    Project Created

---
+++ backend/src/test/java/com/lacsty/lcymovie/LcymovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lacsty.lcymovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcymovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
