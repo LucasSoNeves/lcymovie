7d0f8950f65dde94e7a8526111abaf3afc702a0c
LucasSoNeves <Lucas.neves.ead@gmail.com>
2022-01-15 08:21:36 -0300

    Show score

--- frontend/src/Components/MovieCard/index.tsx
+++ frontend/src/Components/MovieCard/index.tsx
@@ -13,7 +13,8 @@ function MovieCard({ movie }: Props) {
             <img className="lcymovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="lcymovie-card-bottom-container">
                 <h3>{movie.title}</h3>
-                <MovieScore />
+                <MovieScore count={movie.count} score={movie.score} />
+                
                 <Link to={`/form/${movie.id}`}>
                     <div className="btn btn-primary lcymovie-btn">Avaliar</div>
                 </Link>
--- frontend/src/Components/MovieScore/indes.tsx
+++ frontend/src/Components/MovieScore/indes.tsx
@@ -1,15 +1,17 @@
 import MovieStars from "Components/MovieStars";
 import './styles.css';
 
-function MovieScore() {
+type Props = {
+    score: number;
+    count: number
+}
 
-    const score = 3.5;
-    const count = 13;
+function MovieScore({ score, count } : Props) {
 
     return (
         <div className="lcymovie-score-container">
             <p className="lcymovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <MovieStars />
+            <MovieStars score={score} />
             <p className="lcymovie-score-count">{count} avaliações</p>
         </div>
     );
