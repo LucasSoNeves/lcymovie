286a31ba6888cd48ad2c552be4f7b92fa4816588
LucasSoNeves <Lucas.neves.ead@gmail.com>
2022-01-15 09:33:45 -0300

    Pagination

--- frontend/src/Components/Pagination/index.tsx
+++ frontend/src/Components/Pagination/index.tsx
@@ -1,20 +1,26 @@
 import { ReactComponent as Arrow } from 'assets/img/Arrow.svg';
+import { MoviePage } from 'types/movie';
 import './styles.css';
 
-function Pagination() {
+type Props = {
+    page: MoviePage;
+    onChange: Function;
+}
+
+function Pagination({ page, onChange }: Props) {
 
     return (
         <div className="lcymovie-pagination-container">
-    <div className="lcymovie-pagination-box">
-        <button className="lcymovie-pagination-button" disabled={true} >
-            <Arrow />
-        </button>
-        <p>{`${1} de ${3}`}</p>
-        <button className="lcymovie-pagination-button" disabled={false} >
-            <Arrow className="lcymovie-flip-horizontal" />
-        </button>
-    </div>
-</div>
+            <div className="lcymovie-pagination-box">
+                <button className="lcymovie-pagination-button" disabled={page.first} onClick={() => onChange(page.number - 1)} >
+                    <Arrow />
+                </button>
+                <p>{`${page.number} de ${page.totalPages}`}</p>
+                <button className="lcymovie-pagination-button" disabled={page.last} onClick={() => onChange(page.number + 1)} >
+                    <Arrow className="lcymovie-flip-horizontal" />
+                </button>
+            </div>
+        </div>
     );
 }
 
